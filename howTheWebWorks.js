c4d861caf8c9549c01aad5611e8fcf54c506cba3
Elvis Lewis <Elvis5378@Gmail.com>
2023-04-03 17:41:35 -0400

    Init

---
+++ howTheWebWorks.js
@@ -0,0 +1,24 @@
+//Part 1
+HTTP- hypertext transfer protocol; it is the Protocol use between browsers and servers to communicate.
+
+URL- Uniform Resource Locator; it is the address used to locate Data, resources, webpages, ect.
+
+DNS-Domain Name System; it is the system that takes domain names and finds the Corresponding IP address.
+
+query string- it is used in the URL to search for a resource inside a webpage.
+
+Get VS Post- a Get request has no change on the server while the post request has an affect on the server.
+
+HTTP request- it is a request from Client to the server.
+
+HTTP response- is the response from the server to the HTTP request.
+
+HTTP header- consists of request header and response header. They contain information like status, request type, ect
+
+-process of requesting a webpage-
+    -the IP address of the URL is located by DNS
+    -a request is sent to the serve at that IP address
+    -the server response to the request if there's no errors.
+    -the response includes HTML and data needed to build DOM
+    -other requests and responses are sent to resources located in other servers.
+    
